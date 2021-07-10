32ca91bcb29c3eb5eeee50f1c8e8c4cac8d9759f
huynhthao12a3 <huynhthao12a3@gmail.com>
2021-07-10 14:00:07 +0700

    fix header, edit carousel & about

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,50 +24,64 @@ AOS.init({
      duration: 1200,
 })
 
+// $('#carouselExampleIndicators').carousel();
+
+// const sliderItem = document.querySelectorAll('.slider-item h3');
+// console.log(sliderItem)
+// $('.slider-item').onload = function(){
+//      let current = '';
+//      sliderItem.forEach(item => {
+//           item.classList.remove('animate__animated animate__fadeInDown');
+//           if(item.classList.contains(current)){
+//                item.classList.add('animate__animated animate__fadeInDown');
+//           }
+//      })()
+// }
 
 
 // Slider
-$('.home-slider').owlCarousel({
-          loop:true,
-          autoplay:true,
-          nav:true,
-          margin:10,
-          autoplayHoverPause:true,
-          items:1,
-          animationOut: 'fadeOut',
-          animationIn: 'fadeIn',
-          navText: ["<span class='fas fa-chevron-circle-left'></span>","<span class='fas fa-chevron-circle-right'></span>"],
-          responsive:{
-                    0:{
-                              items: 1,
-                              nav: false,
-                    },
-                    600:{
-                              items: 1,
-                              nav: false,
-                    },
-                    1000:{
-                              items: 1,
-                              nav: true,
-                    }
-          }
-});
+// $('.home-slider').owlCarousel({
+//           loop:true,
+//           autoplay:true,
+//           nav:true,
+//           margin:10,
+//           autoplayHoverPause:true,
+//           items:1,
+//           animationOut: 'fadeOut',
+//           animationIn: 'fadeIn',
+//           navText: ["<span class='fas fa-chevron-circle-left'></span>","<span class='fas fa-chevron-circle-right'></span>"],
+//           responsive:{
+//                     0:{
+//                               items: 1,
+//                               nav: false,
+//                     },
+//                     600:{
+//                               items: 1,
+//                               nav: false,
+//                     },
+//                     1000:{
+//                               items: 1,
+//                               nav: true,
+//                     }
+//           }
+// });
 
 // Highlight scroll to section
 const sections=document.querySelectorAll('section');
 const navLi = document.querySelectorAll('nav .container ul li a');
 
-const heightHeader = $('#header').outerHeight();
-const heightHeaderNavFix = $('#header__navbar-fixed').outerHeight();
-
+const heightHeader = $('#home').outerHeight();
+const heightHeaderNavFix = $('.header__navbar').outerHeight();
+console.log(heightHeader)
+console.log(heightHeaderNavFix)
 window.addEventListener('scroll', () => {
      let current = '';
      
      sections.forEach(section => {
           const sectionTop = section.offsetTop;
+console.log(pageYOffset)
 console.log(sectionTop)
-
-          if(pageYOffset >= (sectionTop-(heightHeader+heightHeaderNavFix))){
+          if(pageYOffset >= (sectionTop  - heightHeaderNavFix)){
                current = section.getAttribute('id');
           }
      })
@@ -75,6 +89,13 @@ console.log(sectionTop)
           li.classList.remove('active');
           if(li.classList.contains(current)){
                li.classList.add('active');
-          }
+          }               
      })
+     // navLi.forEach(li =>{
+     //      if(li.classList.contains('active')){
+     //           $('.home').removeClass('active');
+     //      } else {
+     //           $('.home').addClass('active');
+     //      }
+     // })
 })
